a7e5af6379cc3012bccf1ddc5ed01fa6c039a48b
greenish0902 <greenish0902@gmail.com>
2021-09-29 23:10:44 +0900

    CH01 number

--- python-book/CH01/5_convert_dec_to_any_base_rec.py
+++ python-book/CH01/5_convert_dec_to_any_base_rec.py
@@ -1,10 +1,22 @@
+# mycode
 def convert_dec_to_any_base_rec(number, base):
-  convertString = "012345679ABCDEF"
+  num = []
+  while (number >= base):  
+    num.append(number % base)
+    number = number // base
+  num.append(number)
+  return ''.join(list(map(str,num[::-1])))
+  
+# book
+def convert_dec_to_any_base_rec(number, base):
+  convertString = "012345679ABCDE"
   if number < base:
     return convertString[number]
   else:
     return convert_dec_to_any_base_rec(number // base, base) \
         + convertString[number % base]
+
+# test code
 def test_convert_dec_to_any_base_rec():
   number = 9
   base = 2
