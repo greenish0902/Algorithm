e698267c0544b248a2a00cc17d46ce08dd3016ba
greenish0902 <greenish0902@gmail.com>
2021-10-18 21:02:11 +0900

    daily problem solving

---
+++ LeetCode-python3/1859.py
@@ -0,0 +1,15 @@
+class Solution:
+  def sortSentence(self, s: str) -> str:
+    # arr = s.split()
+    new_arr = [0] * len(arr)
+    for i in s.split():
+      key = int(i[-1])
+      new_arr[key-1] = i[:-1]
+    return ' '.join(new_arr)
+
+# class Solution:
+#     def sortSentence(self, s: str) -> str: 
+#         return " ".join(i[:-1] for i in sorted(s.split(), key=lambda x: x[-1]))
+
+test = Solution()
+print(test.sortSentence("is2 sentence4 This1 a3"))
