105991fde8de74cd1bd3eff0ebe688da37ed36ea
greenish0902 <greenish0902@gmail.com>
2022-06-09 23:46:45 +0900

    Add 1475: Implementation (simple)

---
+++ boj-js/Implementation/1475.js
@@ -0,0 +1,27 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const solution = () => {
+  const N = input();
+  let numbers = { ...new Array(10).fill(0) };
+  N.split("").forEach((num) => numbers[num]++);
+  const cnt = Math.max(
+    ...Object.values({ ...numbers, 6: 0, 9: 0 }),
+    Math.min(Math.ceil((numbers[6] + numbers[9]) / 2))
+  );
+  return cnt;
+};
+
+let _line = 0;
+const input = () => stdin[_line++];
+
+let stdin = [];
+rl.on("line", function (line) {
+  stdin.push(line);
+}).on("close", function () {
+  console.log(solution());
+  process.exit();
+});
